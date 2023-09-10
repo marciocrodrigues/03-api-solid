5e80881a24f6290605211bac8ff8fb287e1bb617
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-09-10 17:58:35 -0300

    chore: configuracao inial prismaorn

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    email: 'teste@teste.com.br',
+    name: 'teste',
+  },
+})
 
 export const app = fastify()
